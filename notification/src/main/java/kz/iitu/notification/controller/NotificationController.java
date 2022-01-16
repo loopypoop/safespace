d99e2f54b1a4af70f5dfc56619f514b74ca47f69
Adil <adil.kenesbek21@gmail.com>
2022-01-16 15:04:13 +0600

    firebase removed

--- notification/src/main/java/kz/iitu/notification/controller/NotificationController.java
+++ notification/src/main/java/kz/iitu/notification/controller/NotificationController.java
@@ -2,7 +2,6 @@ package kz.iitu.notification.controller;
 
 import com.google.firebase.messaging.Message;
 import kz.iitu.notification.model.DirectNotification;
-import kz.iitu.notification.service.impl.FCMService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,13 +9,5 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class NotificationController {
-
-    @Autowired
-    private FCMService fcm;
-
-    @PostMapping("/notification")
-    public void sendTargetNotification(@RequestBody DirectNotification notification) {
-        fcm.sendNotificationToTarget(notification);
-    }
 
 }
